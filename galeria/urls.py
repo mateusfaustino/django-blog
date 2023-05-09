fd40f12cffd6ef1a9d56a6ad904aa4513f2d7d83
Mateus Faustino <mateusfaustino2017@gmail.com>
2023-05-09 17:50:24 -0300

    delete galeria app

--- galeria/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from galeria.views import index,image
-
-urlpatterns = [
-    path('', index, name='home'),
-    path('image', image, name='image'),
-]
--- galeria/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request,'galeria/index.html')
-
-def image(request):
-    return render(request,'galeria/imagem.html')
