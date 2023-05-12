6aa27d738b9e14f49b78a80a2a0e56bbf21690c5
Mateus Faustino <mateusfaustino2017@gmail.com>
2023-05-12 15:21:03 -0300

    add Model and Migrations for Posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    excerpt = models.TextField(max_length=200)
+    # thumb = 
+    # author = 
