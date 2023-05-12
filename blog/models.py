ca4da2006300ebb3a4f5d5c239de36605f0ed838
Mateus Faustino <mateusfaustino2017@gmail.com>
2023-05-12 15:48:46 -0300

    add ManagerConfig instead manager app

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, null=False, blank=False)
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     excerpt = models.TextField(max_length=200)
+
+    def __str__(self):
+        return f'Post [title={self.title}]'
     # thumb = 
     # author = 
