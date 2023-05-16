e8469e7188174346688cd3f7694d405401ca4cfd
Mateus Faustino <mateusfaustino2017@gmail.com>
2023-05-16 09:14:27 -0300

    fix blogconfig

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def home(request):
 
@@ -26,32 +27,34 @@ def home(request):
         },
 	]
 
-    posts = [
-        # {
-        #     'title':'',
-        #     'excerpt': '',
-        #     'thumb':'',
-        #     'link':''
-        # }
-        {
-            'title':'Descubra as melhores atividades para aproveitar ao máximo sua viagem ao Rio de Janeiro',
-            'excerpt': 'O Rio de Janeiro é uma cidade repleta de atrações turísticas e com uma rica cultura brasileira. Com suas belas praias, morros imponentes e edifícios históricos, é um dos destinos mais procurados por turistas nacionais e internacionais. Se você está planejando uma viagem ao Rio, aqui estão algumas sugestões do que fazer:',
-            'thumb':'https://latrilha.com.br/wp-content/uploads/2023/04/53.jpg',
-            'link':'https://latrilha.com.br/2023/04/14/descubra-as-melhores-atividades-para-aproveitar-ao-maximo-sua-viagem-ao-rio-de-janeiro/'
-        },
-        {
-            'title':'8 dicas essenciais para fotografar paisagens e animais selvagens durante trilhas e acampamentos',
-            'excerpt': 'Fotografar paisagens e animais selvagens durante trilhas e acampamentos pode ser uma experiência emocionante e gratificante. Com a paisagem natural como pano de fundo, é fácil capturar imagens impressionantes que serão apreciadas por anos. Aqui estão algumas dicas de fotografia que podem ajudá-lo a tirar fotos incríveis de paisagens e animais selvagens durante trilhas e camping:',
-            'thumb':'https://latrilha.com.br/wp-content/uploads/2023/04/51.jpg',
-            'link':'https://latrilha.com.br/2023/04/01/8-dicas-essenciais-para-fotografar-paisagens-e-animais-selvagens-durante-trilhas-e-acampamentos/'
-        },
-        {
-            'title':'Ecoturismo e conservação ambiental: Como viajantes podem contribuir para a preservação da natureza durante trilhas e viagens',
-            'excerpt': 'Ecoturismo e conservação ambiental: Informações sobre a importância da preservação ambiental durante as trilhas e viagens, e como os viajantes podem contribuir para a conservação da natureza.',
-            'thumb':'https://latrilha.com.br/wp-content/uploads/2023/03/latrilha-2.jpg',
-            'link':'https://latrilha.com.br/2023/03/26/ecoturismo-e-conservacao-ambiental-como-viajantes-podem-contribuir-para-a-preservacao-da-natureza-durante-trilhas-e-viagens/'
-        }
-    ]
+    # posts = [
+    #     # {
+    #     #     'title':'',
+    #     #     'excerpt': '',
+    #     #     'thumb':'',
+    #     #     'link':''
+    #     # }
+    #     {
+    #         'title':'Descubra as melhores atividades para aproveitar ao máximo sua viagem ao Rio de Janeiro',
+    #         'excerpt': 'O Rio de Janeiro é uma cidade repleta de atrações turísticas e com uma rica cultura brasileira. Com suas belas praias, morros imponentes e edifícios históricos, é um dos destinos mais procurados por turistas nacionais e internacionais. Se você está planejando uma viagem ao Rio, aqui estão algumas sugestões do que fazer:',
+    #         'thumb':'https://latrilha.com.br/wp-content/uploads/2023/04/53.jpg',
+    #         'link':'https://latrilha.com.br/2023/04/14/descubra-as-melhores-atividades-para-aproveitar-ao-maximo-sua-viagem-ao-rio-de-janeiro/'
+    #     },
+    #     {
+    #         'title':'8 dicas essenciais para fotografar paisagens e animais selvagens durante trilhas e acampamentos',
+    #         'excerpt': 'Fotografar paisagens e animais selvagens durante trilhas e acampamentos pode ser uma experiência emocionante e gratificante. Com a paisagem natural como pano de fundo, é fácil capturar imagens impressionantes que serão apreciadas por anos. Aqui estão algumas dicas de fotografia que podem ajudá-lo a tirar fotos incríveis de paisagens e animais selvagens durante trilhas e camping:',
+    #         'thumb':'https://latrilha.com.br/wp-content/uploads/2023/04/51.jpg',
+    #         'link':'https://latrilha.com.br/2023/04/01/8-dicas-essenciais-para-fotografar-paisagens-e-animais-selvagens-durante-trilhas-e-acampamentos/'
+    #     },
+    #     {
+    #         'title':'Ecoturismo e conservação ambiental: Como viajantes podem contribuir para a preservação da natureza durante trilhas e viagens',
+    #         'excerpt': 'Ecoturismo e conservação ambiental: Informações sobre a importância da preservação ambiental durante as trilhas e viagens, e como os viajantes podem contribuir para a conservação da natureza.',
+    #         'thumb':'https://latrilha.com.br/wp-content/uploads/2023/03/latrilha-2.jpg',
+    #         'link':'https://latrilha.com.br/2023/03/26/ecoturismo-e-conservacao-ambiental-como-viajantes-podem-contribuir-para-a-preservacao-da-natureza-durante-trilhas-e-viagens/'
+    #     }
+    # ]
+
+    posts = Post.objects.all()
 
     hero_texts = [
         'La Trilha',
