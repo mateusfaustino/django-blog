7df3e90aed21b553fda0df202b187f711a23aa00
Mateus Faustino <mateusfaustino2017@gmail.com>
2023-05-23 12:36:24 -0300

    Integrating Tinymce editor in django admin

--- post/admin.py
+++ post/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-
 from post.models import Post
+from .forms import PostAdminForm
 
 class ListPosts(admin.ModelAdmin):
     list_display = ('id','title','excerpt','published', )
     list_display_links = ('id','published','title', 'excerpt' )
     search_fields = ('title',)
+    form = PostAdminForm
+
+
 
 admin.site.register(Post,ListPosts)
